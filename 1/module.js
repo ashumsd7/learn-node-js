259246fad708aaddb448bdeb8ca0a10ef55602ae
aathym <aat8001@gmail.com>
2023-01-04 18:05:27 +0530

    fs modules, create directories

--- 1/module.js
+++ 1/module.js
@@ -1,37 +1,52 @@
 // npm modules
-const color = require("cli-color");
+// const color = require("cli-color");
 
-(function (name) {
-  var x = 12;
-  console.log(">>>>>>>>>>", name);
-})("Ashu");
+// (function (name) {
+//   var x = 12;
+//   console.log(">>>>>>>>>>", name);
+// })("Ashu");
 
 // console.log(x)
 
-console.log(color.bgBlue("Namaste"));
+// console.log(color.bgBlue("Namaste"));
 
 //local module
 
-const auth = require("./auth");
+// const auth = require("./auth");
 
-console.log(auth);
-auth.register("Ashutosh");
-auth.login("username", "*****");
+// console.log(auth);
+// auth.register("Ashutosh");
+// auth.login("username", "*****");
 
-//code modules
+// //code modules
 
 const path = require("path");
-const { dirname } = require("path");
+// const { dirname } = require("path");
 
-//path gives multiple functions
+// //path gives multiple functions
 
-// get absolute path
-console.log(path.dirname(__filename), 'foldername')
-//get file name
-console.log(path.basename(__filename), 'file name')
-//get extension
-console.log(path.extname(__filename), 'extension name')
-//parse
-console.log(path.parse(__filename), 'lets parse')
-//join ( and get url)
-console.log(path.join(__dirname,'order'), 'join')
+// // get absolute path
+// console.log(path.dirname(__filename), 'foldername')
+// //get file name
+// console.log(path.basename(__filename), 'file name')
+// //get extension
+// console.log(path.extname(__filename), 'extension name')
+// //parse
+// console.log(path.parse(__filename), 'lets parse')
+// //join ( and get url)
+// console.log(path.join(__dirname,'order'), 'join')
+
+//FIle system module
+
+// fs
+
+const fs = require("fs");
+
+fs.mkdir(path.join(__dirname, "/test"), (err) => {
+  if (err) {
+    console.log("ERROR",err);
+    return;
+  }
+
+  console.log("folder created");
+});
