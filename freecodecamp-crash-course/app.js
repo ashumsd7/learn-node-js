5fe4faf5f33d8e6bf7643658e6f72142e4c794e4
aathym <aat8001@gmail.com>
2023-01-05 14:07:44 +0530

    alternative way of export

--- freecodecamp-crash-course/app.js
+++ freecodecamp-crash-course/app.js
@@ -1,10 +1,13 @@
 // modules
 
-const {john, peter} = require("./4-names");
-const sayHi= require('./5-utils')
 
+// const {john, peter} = require("./4-names");
+// const sayHi= require('./5-utils')
+// const data= require('./6-alternative-flavor')
+// console.log(data)
+require('./7-mind-grenade')
 // console.log(names)
 
-sayHi("susan");
-sayHi(peter);
-sayHi(john);
+// sayHi("susan");
+// sayHi(peter);
+// sayHi(john);
