5053dfe732503d500745ea412f56457cba71a00f
aathym <aat8001@gmail.com>
2023-01-05 14:12:27 +0530

    alternative way of export

---
+++ freecodecamp-crash-course/6-alternative-flavor.js
@@ -0,0 +1,7 @@
+module.exports.items = [1, 2, 3, 4];
+
+const person = {
+  name: "bob",
+};
+
+module.exports.singlePerson = person
---
+++ freecodecamp-crash-course/7-mind-grenade.js
@@ -0,0 +1,9 @@
+const num1 = 5;
+const num2 = 10;
+
+function addValues(n1, n2) {
+  console.log(`the sum is : ${n1 + n2}`);
+}
+
+
+addValues(num1,num2)
