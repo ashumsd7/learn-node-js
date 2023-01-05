cbe56bd07f1273d9e6091c23af99b57fefcce02f
aathym <aat8001@gmail.com>
2023-01-05 12:22:53 +0530

    globles

---
+++ freecodecamp-crash-course/1-intro.js
@@ -0,0 +1,18 @@
+//globals ( no window)
+
+console.log(__dirname);
+
+setTimeout(() => {
+  console.log("hii");
+
+setTimeout(() => {
+    console.log("hii 44");
+  }, 4000);
+}, 5000);
+
+
+setTimeout(() => {
+    console.log("hii 444 bahar");
+  }, 4000);
+
+  
